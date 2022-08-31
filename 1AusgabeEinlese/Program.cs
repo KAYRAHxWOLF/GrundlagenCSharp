f25ae09e08edd7b90861ee7e85fbbb4d0b555142
Darkesemble _YT <dakiesemble@gmail.com>
2022-08-31 10:46:38 +0300

    fehler gefixt

---
+++ 1AusgabeEinlese/Program.cs
@@ -0,0 +1,53 @@
+﻿//aufgabe 1
+
+
+using System.Diagnostics.Metrics;
+
+Console.WriteLine("──────▄▀▄─────▄▀▄");
+Console.WriteLine("─────▄█░░▀▀▀▀▀░░█▄");
+Console.WriteLine("─▄▄──█░░░░░░░░░░░█──▄▄");
+Console.WriteLine("█▄▄█─█░░▀░░┬░░▀░░█─█▄▄█");
+Console.WriteLine(" miau!");
+
+
+//aufgabe 2
+
+
+string vorname2 = "Nurullah";
+string nachname2 = "Yilmaz";
+string alter2 = "18";
+
+Console.WriteLine(vorname2);
+Console.WriteLine(nachname2);
+Console.WriteLine(alter2);
+
+
+
+//aufgabe 3 
+
+
+
+string stadt;
+string plz;
+string vorname;
+string nachname;
+string alter;
+
+
+Console.Write("Bitte ORT eingeben ");
+stadt = Console.ReadLine();
+Console.Write("Bitte Postleitzahl eingeben ");
+plz = Console.ReadLine();
+Console.Write("Bitte dein Vorname eingeben");
+vorname = Console.ReadLine();
+Console.Write("Bitte dein Nachname eingeben ");
+nachname = Console.ReadLine();
+Console.Write("Bitte dein Alter eingeben");
+alter = Console.ReadLine();
+
+
+Console.WriteLine(vorname);
+Console.WriteLine(nachname);
+Console.WriteLine(plz);
+Console.WriteLine(stadt);
+Console.WriteLine(alter);
