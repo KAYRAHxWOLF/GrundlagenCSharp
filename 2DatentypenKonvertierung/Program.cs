ef27cab40a0bc4c366052532ef523b56414eeacb
Darkesemble _YT <dakiesemble@gmail.com>
2022-08-31 10:43:46 +0300

    erneuert

--- 2DatentypenKonvertierung/Program.cs
+++ 2DatentypenKonvertierung/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//aufgabe 1
+
+
+using System.Diagnostics.Metrics;
+
+Console.WriteLine("──────▄▀▄─────▄▀▄");
+Console.WriteLine("─────▄█░░▀▀▀▀▀░░█▄");
+Console.WriteLine("─▄▄──█░░░░░░░░░░░█──▄▄");
+Console.WriteLine("█▄▄█─█░░▀░░┬░░▀░░█─█▄▄█");
+Console.WriteLine(" miau!");
+
+
+//aufgabe 2
+
+
+string vorname2 = "Nurullah";
+string nachname2 = "Yilmaz";
+string alter2 = "18";
+
+Console.WriteLine(vorname2);
+Console.WriteLine(nachname2);
+Console.WriteLine(alter2);
+
+
+
+//aufgabe 3 
+
+
+
+string stadt;
+string plz;
+string Vorname;
+string Nachname;
+string Alter;
+
+
+Console.Write("Bitte ORT eingeben ");
+stadt = Console.ReadLine();
+Console.Write("Bitte Postleitzahl eingeben ");
+plz = Console.ReadLine();
+Console.Write("Bitte dein Vorname eingeben");
+Vorname = Console.ReadLine();
+Console.Write("Bitte dein Nachname eingeben ");
+Nachname = Console.ReadLine();
+Console.Write("Bitte dein Alter eingeben");
+Alter = Console.ReadLine();
+
+
+Console.WriteLine(vorname);
+Console.WriteLine(nachname);
+Console.WriteLine(plz);
+Console.WriteLine(stadt);
+Console.WriteLine(alter);
